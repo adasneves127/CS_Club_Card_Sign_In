786df63a85b0d27b794f7ec392634777f46c67c9
adasneves127 <adasneves127@gmail.com>
2023-10-24 16:57:50 -0400

    Added GUI Elements

--- card_utils.py
+++ card_utils.py
@@ -12,3 +12,15 @@ def hash_card_id(cardID: str) -> str:
         ]
     )
     return hashlib.sha512(cardID.encode()).hexdigest()
+
+
+def get_sign_in_id(cardID: str) -> str:
+    cardID = ''.join(
+        [
+            x
+            for x
+            in cardID
+            if x.isdigit()
+        ]
+    )
+    return hashlib.sha256(cardID.encode()).hexdigest()[:32]
---
+++ tex_main.py
@@ -0,0 +1,84 @@
+import textual.containers as cons
+import textual.widgets as widgets
+from textual.screen import Screen
+from textual.app import App, ComposeResult
+from card_utils import hash_card_id, get_sign_in_id
+from db_conn import conn
+import mysql.connector.errors as sqlErrors
+
+
+class UserNotFound(Screen):
+    BINDINGS = [("escape", "app.pop_screen", "Pop screen"), 
+                ("enter", "app.pop_screen", "Pop Screen")]
+
+    def compose(self) -> ComposeResult:
+        yield widgets.Static(" ERROR ", id="title")
+        yield widgets.Static(
+            "User not Found as an authorized user in SQL Database!"
+            )
+        yield widgets.Static("Press enter to continue [blink]_[/]",
+                             id="any-key")
+
+
+class Input(widgets.Input):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = ""
+
+    def on_input_changed(self, changed_input: widgets.Input):
+        self.data = changed_input.value
+
+
+# Make the main screen
+class CS_Sign_In(App):
+    CSS_PATH = "main.tcss"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.db = None
+        self.cur = None
+
+    def compose(self) -> ComposeResult:
+        # Add a header
+        yield widgets.Header(show_clock=True)
+        with widgets.TabbedContent(
+                                "Log In" if self.db is None
+                                else "Maintain Users"):
+            if self.db is None:
+                with widgets.TabPane("Log In"):
+                    with cons.Container(classes="Heading"):
+                        yield widgets.Static("Log Into Database")
+                    # Add a username box
+                    yield widgets.Label("Username: ")
+                    yield Input(password=True, id="UBUN")
+                    yield widgets.Label("Password: ")
+                    yield Input(password=True, id="DBPW")
+                    yield widgets.Button("Log In", id="logIn")
+
+            with widgets.TabPane("Maintain Users"):
+                yield widgets.Static("Test 1")
+            with widgets.TabPane("Maintain Officers"):
+                yield widgets.Static("Test2")
+            with widgets.TabPane("Search"):
+                yield widgets.Static("Test3")
+            with widgets.TabPane("Sign In"):
+                yield widgets.Static("Test4")
+
+    def on_button_pressed(self, event: widgets.Button.Pressed):
+        if event.button.id == 'logIn':
+            username = self.get_widget_by_id('UBUN').value  # type: ignore
+            password = self.get_widget_by_id("DBPW").value  # type: ignore
+            username = get_sign_in_id(username)
+            try:
+                self.db = conn(
+                                "csclub-bridgew.mysql.database.azure.com",
+                                username,
+                                password)
+                self.cur = self.db.cursor
+                self.
+            except sqlErrors.ProgrammingError:
+                self.push_screen(UserNotFound())
+                
+
+if __name__ == "__main__":
+    CS_Sign_In().run()
